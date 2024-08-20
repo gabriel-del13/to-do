67052b230da5466a7da6596ad86c82473ae2242a
Gabriel <gaby13453@gmail.com>
2024-08-19 20:42:27 -0500

    background

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
+from tkinter import Tk, Label
+from PIL import Image, ImageTk
 import tkinter as tk
 
 window = tk.Tk()
-window.geometry("500x500")
+window.geometry("700x400")
+window.resizable(False, False)
 
+background= Image.open('zero_two.jpg')
+background = background.resize((700, 400), Image.LANCZOS)
+background_tk = ImageTk.PhotoImage(background)
+
+background_label = Label(window, image=background_tk)
+background_label.place (x=0, y=0, relwidth = 1, relheight=1)
 window.mainloop()
